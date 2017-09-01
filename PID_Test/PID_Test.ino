bcb4bec38862640e4675fbbf59de37b62aae5a49
peter-nisbet <peternisbet265@live.ca>
2017-09-01 09:00:38 -0400

    Added my own PID algorithm
    PID algorithm created by Peter Nisbet.

--- PID_Test/PID_Test.ino
+++ PID_Test/PID_Test.ino
@@ -25,7 +25,7 @@ int speedVal3 = 0;    //Speed value for input fan
 long entry = 0;
 
 //Change time of polling system in ms intervals.
-unsigned long timeVal = 10000;  
+unsigned long timeVal = 5000;  
 
 char fanSpeed[13];
 
@@ -42,6 +42,23 @@ void fanControlLoop();
 void PID_init();
 void PID_Control();
 
+
+//Custom PID Code Variables
+float current_position = 0;
+float error = 0;
+float integral = 0;
+float derivative = 0;
+float pwm_out = 0;
+float target_position = 23.2;
+float last_error = 0;
+int x = 0;
+
+float kp = 3;
+float ki = 1;
+float kd = 1;
+void PID_Loop();
+
+
 void setup() {
 
   Serial.begin(9600); //Serial port begin at 9600 baud
@@ -54,7 +71,7 @@ void setup() {
 
   //Begin Silabs sensor
   sensor.begin();
-  PID_init();
+  //PID_init();
 }
 
 void loop() {
@@ -66,7 +83,8 @@ void loop() {
   //entry++;
   //printText();
 
-  PID_Control();
+  //PID_Control();
+  PID_Loop();
 
   delay(timeVal); //Sampling delay
 }
@@ -301,5 +319,75 @@ void PID_Control()
     }
     lastMessage = now;
   }
+}
+
+void PID_Loop()
+{
+  float pwm_pos = 0;
+  int Mot_PWM = 0;
+  current_position = sensor.getTemp();
+
+  error = target_position - current_position;
+
+  integral = integral + error;
+
+  derivative = error - last_error;
+
+  pwm_out = (kp * error) + (ki * integral) + (kd * derivative);
+
+  if(pwm_out>255)
+  {
+    pwm_out = 255;
+  }
+  else if(pwm_out < -255)
+  {
+    pwm_out = -255;
+  }
+
+  if(integral > 0)
+  {
+    integral = 0;
+  }
+  x++;
+  Serial.print("Test Value: ");
+  Serial.println(x);
+  Serial.print("Value of current temperature is: ");
+  Serial.println(current_position);
+  Serial.print("Value of error is: ");
+  Serial.println(error);
+  Serial.print("Value of integral is: ");
+  Serial.println(integral);
+  Serial.print("Value of derivative is: ");
+  Serial.println(derivative);
+  Serial.print("Value of output is: ");
+  Serial.println(pwm_out);
+  Serial.println();
+
+  last_error = error;
+
+  pwm_pos = -(pwm_out);
+
+  if(pwm_pos < 0)
+  {
+    pwm_pos = 0;
+  }
+
+  Serial.println(pwm_pos);
+
+  Mot_PWM = pwm_pos * 10;
+
+  if(Mot_PWM > 255)
+  {
+    Mot_PWM = 255;
+  }
+  else if (Mot_PWM <50 && Mot_PWM != 0)
+  {
+    analogWrite(fan1, 100);
+    delay(1000);  
+    Mot_PWM = 30;
+  }
+
+  //Mot_PWM = 70;
+  analogWrite(fan1, Mot_PWM);  
 }
 
